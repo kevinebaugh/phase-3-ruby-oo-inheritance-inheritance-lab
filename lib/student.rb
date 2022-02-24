daadbd7d41b324e4be3a732dfd0e702aa1f98e43
Kevin Ebaugh <kevinebaugh@gmail.com>
2022-02-23 21:03:26 -0500

    commit tree ad3154c89ac17a2b7a472d84c153509c5f877a1b

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,14 @@
-class Student 
+class Student < User
 
+  def initialize
+    @knowledge = []
+  end
+
+  def learn(something_new)
+    @knowledge.push(something_new)
+  end
+
+  def knowledge
+    @knowledge
+  end
 end
